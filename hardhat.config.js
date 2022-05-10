e57e5abc6ba059b57cc53715c9dfc2b4a0af04d1
sumit03guha <sumit03guha@gmail.com>
2022-05-10 17:45:40 +0530

    inti commit

---
+++ hardhat.config.js
@@ -0,0 +1,56 @@
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-etherscan');
+require('solidity-coverage');
+
+const secrets = require('./secrets.json');
+
+const ETHERSCAN_API_KEY = secrets.ETHERSCAN_API_KEY;
+const BSCSCAN_API_KEY = secrets.BSCSCAN_API_KEY;
+const POLYGONSCAN_API_KEY = secrets.POLYGONSCAN_API_KEY;
+const FTMSCAN_API_KEY = secrets.FTMSCAN_API_KEY;
+
+module.exports = {
+  solidity: {
+    version: '0.8.12',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  /**
+   * @abstract Configure the mocha test timeout for testing the contracts.
+   * @description Most contract functions take a while to execute, especially when making
+   * multiple calls, hence, increasing the timeout enables all the tests to reach completion.
+   * @default 2000 ms
+   */
+  mocha: {
+    timeout: 100000000,
+  },
+  networks: {
+    hardhat: {
+      /**
+       * @see https://hardhat.org/hardhat-network/reference/#allowunlimitedcontractsize
+       * @default false
+       */
+      allowUnlimitedContractSize: true,
+    },
+
+    /**
+     * @description This is the default network for truffle dashboard.
+     * There is no need to paste PRIVATE_KEY for deployment. This enables the connection to the
+     * MetaMask wallet in the browser from where the contract deployment transcation can be signed.
+     */
+    truffle: {
+      url: 'http://localhost:24012/rpc',
+    },
+  },
+  /**
+   * @description The following config requires the @param apiKey to be set
+   * for the verification of the contracts.
+   * */
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+};
